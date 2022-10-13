8ad692e0bd0005ff6d4c8f6f06ce5968d3beae29
t-montes <t.montes@uniandes.edu.co>
2022-10-13 00:28:08 -0500

    add verioning

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
